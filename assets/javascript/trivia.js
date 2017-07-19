2b5601b3e3f9223f848613cafa78daa02493e09f
91integ25 <91integ25@gmail.com>
2017-07-19 19:05:32 -0400

    added description

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -1,7 +1,156 @@
+// // function that waits for document to load before executing javascript within
+// $(document).ready( function(){
+// // time variable
+// var count =5;
+// var incorrect = 0;
+// var correct = 0;
+// var unanswered = 0;
+
+// // array of objects to output to the DOM
+// var questions = [{
+// 	question: "where is your main house?",
+// 	answer:["AL","NY","WV","GA"],
+// 	correct:"NY",
+// 	value:"location"
+
+// },
+// {
+// 	question: "what is your car?",
+// 	answer:["BMW","Toyota","VW","FORD"],
+// 	correct:"VW",
+// 	value:"car"
+// },
+// {
+// 	question: "what is your duck?",
+// 	answer:["mallard","green","goose","swan"],
+// 	correct:"swan",
+// 	value:"duck"
+// },
+// {
+// 	question: "which is your farm?",
+// 	answer:["tyson","pepperidge","jojo","old"],
+// 	correct:"pepperidge",
+// 	value:"farm"
+// }
+
+// ];
+// //appending start button to start page
+// var questionDiv = $("#questions");
+// $("#questions").append("<button>" + "start" + "</button>");
+// $("button").css({"width":"300px","height":"50px","font-size":"30px"});
+
+// function displayQuestions(){
+// for (var i = 0; i < questions.length;i++){
+//  var questionHeader = $("<h3>");
+//  $(questionHeader).append(questions[i].question);
+//  $("#questions").append(questionHeader);
+//  for (var j = 0; j < questions[i].answer.length;j++){
+//     	  var questionAnswer = questions[i].answer[j];
+//     	  var answerBtn = $("<input>");
+//           var answerLabel = $("<label>").text(questionAnswer);
+
+//     $(answerBtn).attr({
+//       "type": "radio",
+//       "name": questions[i].value,
+//       "value": questionAnswer
+//   });
+
+//     $(answerLabel).append(answerBtn);
+//     $("#questions").append(answerLabel);
+
+// function checkAnswers(){
+//     	if(answerBtn.checked === true){
+//     		console.log("hello");
+//     		if(answerBtn.value === questionAnswer ){
+//     			console.log("hey there");
+//     		}
+//     	}
+//     }
+// 	 }
+// }
+
+// }
+
+// // on click button will disappear questions,answers and timer will appear
+// $("button").click(function(){
+// 	$("#questions").empty();
+// 	// creates timer display
+// 	var timerDiv = $("<div>");
+// 	$(timerDiv).text("Time remaining: " + count);
+// 	$("#questions").append(timerDiv);
+	
+	
+	
+// displayQuestions();
+
+
+   
+
+
+
+
+// //  questions.map(function(e, index) { 
+// //  //creates buttons dynamically
+// //   var questionAnswer = e.answer;
+// //   var newForm = $("<form>");
+
+// //   $("#questions").append(newForm);
+
+// //   //creates questions dynamically
+// //   var questionHeader = $("<h3>");
+
+// //   $(questionHeader).append(e.question);
+// //   $(newForm).append(questionHeader);
+// //   $("#answers").append(answerBtn);
+
+// //   // adding buttons dynamically
+// //   for (var i = 0; i < questionAnswer.length; i++) {
+// //     var answerBtn = $("<input>");
+// //     var answerLabel = $("<label>").text(questionAnswer[i]);
+
+// //     $(answerBtn).attr({
+// //       "type": "radio",
+// //       "name": e.value,
+// //       "value": questionAnswer[i]
+
+
+// //     });
+    
+// // 	$(answerLabel).append(answerBtn);
+// //     $(newForm).append(answerLabel);
+
+// //    console.log(count)
+// //   }// end of for loop to display buttons and values
+
+ 
+// // });// end of map function
+
+//  $("#questions").append("<button>" + "done" + "</button>");
+// $("button").css({"width":"300px","height":"50px","font-size":"30px", "margin-top":"30px"});
+//  var countDown = setInterval(function () {
+ 	
+// 	count--;
+// 	$(timerDiv).text("Time remaining: " + count);
+
+// 	if(count <= 0){
+// 		checkAnswers();
+// 			$("#questions").empty();
+			
+// 			$("#questions").append("<p>" + "correct: " + correct + "</p>");
+// 			$("#questions").append("<p>" + "incorrect: " + incorrect + "</p>");
+// 			$("#questions").append("<p>" + "unanswered: " + unanswered + "</p>");
+		
+// 		clearInterval(countDown);
+// 	}
+// }, 1000);
+
+// });
+
+// });
 // function that waits for document to load before executing javascript within
 $(document).ready( function(){
 // time variable
-var count = 30;
+var count = 5;
 var incorrect = 0;
 var correct = 0;
 var unanswered = 0;
@@ -37,17 +186,7 @@ var questions = [{
 var questionDiv = $("#questions");
 $("#questions").append("<button>" + "start" + "</button>");
 $("button").css({"width":"300px","height":"50px","font-size":"30px"});
-
-//on click button will disappear questions,answers and timer will appear
-$("button").click(function(){
-	$("#questions").empty();
-// creates timer display
-	var timerDiv = $("<div>");
-	$(timerDiv).text("Time remaining: " + count);
-	$("#questions").append(timerDiv);
-	
-	
-
+function displayQA(){
  questions.map(function(e, index) { 
  //creates buttons dynamically
   var questionAnswer = e.answer;
@@ -72,37 +211,43 @@ $("button").click(function(){
       "name": e.value,
       "value": questionAnswer[i]
 
-
     });
-
-    $(answerBtn).addClass("values");
-
-    $(answerLabel).append(answerBtn);
+	$(answerLabel).append(answerBtn);
     $(newForm).append(answerLabel);
-   console.log(answerBtn[i]);
+    
+     
+ 
+ 
   }// end of for loop to display buttons and values
+ });// end of map function
+}
 
-});// end of map function
-
- var countDown = setInterval(function () {
+//on click button will disappear questions,answers and timer will appear
+$("button").click(function(){
+	$("#questions").empty();
+// creates timer display
+ 		displayQA();
+ 		
+	var timerDiv = $("<div>");
+	$(timerDiv).text("Time remaining: " + count);
+	$("#questions").append(timerDiv);
+	
+	 var countDown = setInterval(function () {
 	count--;
 	$(timerDiv).text("Time remaining: " + count);
-
+	
 	if(count <= 0){
-			$("#questions").empty();
-			
-			$("#questions").append("<p>" + "correct: " + correct + "</p>");
-			$("#questions").append("<p>" + "incorrect: " + incorrect + "</p>");
-			$("#questions").append("<p>" + "unanswered: " + unanswered + "</p>");
+	$("#questions").empty();
+	$("#questions").append("<p>" + "correct: " + correct + "</p>");
+	$("#questions").append("<p>" + "incorrect: " + incorrect + "</p>");
+	$("#questions").append("<p>" + "unanswered: " + unanswered + "</p>");
 		
 		clearInterval(countDown);
-	}
+	} 
 }, 1000);
- console.log(correct);
- console.log(incorrect);
- console.log(unanswered);
 
 
 });
 
 });
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Trivia Game 
+
+In this application you are given a questionnaire which you must complete in a given time frame then the correct and incorrect answers are displayed back to you at the end.
+
+Application that allowed me to learn about javascript timers they were challenging and allowed me to also learn how callbacks worked.
